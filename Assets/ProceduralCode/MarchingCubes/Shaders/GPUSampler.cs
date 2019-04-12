cd3eb9371171772a58e8c3a6092f7518ad689f2e
Tyler Shingler <tyshingler@gmail.com>
2019-04-12 19:39:02 -0400

    Revert "- Looking to revert"
    This reverts commit cc01947461ccf8c15ae59067453072527597a010.

--- Assets/ProceduralCode/MarchingCubes/Shaders/GPUSampler.cs
+++ Assets/ProceduralCode/MarchingCubes/Shaders/GPUSampler.cs
@@ -113,8 +113,7 @@ public class GPUSampler
     private LerpField SetLerpFieldArrayForDirection(int lerpFieldHandler, string arrayName)
     {
         LerpField result = new LerpField();
-        //Need to add one b/c the 17th layers are needed to make meshes.
-        result.lerpValues = new float[(volumeSize+1) * (volumeSize+1)  * (volumeSize+1)];
+        result.lerpValues = new float[volumeSize * volumeSize * volumeSize];
         result.lerpBuffer = new ComputeBuffer(result.lerpValues.Length, sizeof(float));
         lerpFieldGenerator.SetBuffer(lerpFieldHandler, arrayName, result.lerpBuffer);
         return result;
